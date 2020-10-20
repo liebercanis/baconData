d0e8a87526a3c81453a97b8db05d668612f24342
liebercanis <mgold@unm.edu>
2020-10-20 16:52:05 -0600

    compiled read raw

---
+++ compiled/convertRaw.cc
@@ -0,0 +1,123 @@
+//////////////////////////////////////////////////////////
+#include <sstream>
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <valarray>
+#include <TString.h>
+#include <TROOT.h>
+#include <TVirtualFFT.h>
+#include <TChain.h>
+#include <TTree.h>
+#include <TMath.h>
+#include <TNtuple.h>
+#include <TFile.h>
+#include <Rtypes.h>
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TF1.h>
+#include <TFormula.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include "TString.h"
+#include "TObjString.h"
+#include "TSystemDirectory.h"
+#include "TFile.h"
+#include "TBRawRun.hxx"
+
+TBRawRun *brun;
+vector<ifstream*> streams;
+
+using namespace std;
+
+void readEvent() 
+{
+  char line[256];
+
+  for(unsigned is =0; is<streams.size(); ++is ) {
+    TBRawEvent* rawEv = brun->detList[is];
+    //cout << is << " for "  << rawEv->description  << endl;
+    while( streams[is]->good() ) {
+      streams[is]->getline(line,256);
+      TString tline(line);
+      TObjArray* tokenArray = tline.Tokenize(' ');
+      if(tokenArray->GetEntries()>1) {
+        TObjString *sv = (TObjString*) tokenArray->At(0);
+        if(sv->GetString()==TString("DC") && rawEv->digi.size()>0 ) {
+          //cout << " end of event " << rawEv->event << "  " << rawEv->GetName() << endl;
+          break;
+        }
+      } else {
+        TObjString *sv = (TObjString*) tokenArray->At(0);
+        if(sv==NULL) continue;
+        rawEv->event = 1;
+        rawEv->digi.push_back(sv->GetString().Atof());
+      }
+    }
+    //printf (" readEvent for %s digis  %lu \n",rawEv->GetName(),rawEv->digi.size());
+  }
+  brun->fill();
+  brun->detListClear();
+}
+
+
+unsigned openFiles() 
+{
+  TString dirName(Form("data/%s",brun->GetName()));
+  cout << "dirName " << dirName << endl;
+  TSystemDirectory dir("rawdir",dirName);
+  TList *files = dir.GetListOfFiles();
+
+  TIter next(files);
+  TSystemFile *file;
+  while( (file = (TSystemFile*) next()) ) {
+    string name = string(file->GetName());
+    string exten  = name.substr( name.find_last_of(".")+1 );
+    if(exten!=string("txt")) continue;
+    string tag = name.substr( 0, name.find_last_of(".") );
+    string fullName =  string( dirName.Data())  + string("/")+name;
+    ifstream* in = new ifstream(fullName,std::ios::in);
+    if(in->is_open()) streams.push_back(in);
+    brun->detList[streams.size()-1]->description=TString(name.c_str());
+    TBranch *branch = brun->btree->GetBranch(brun->detList[streams.size()-1]->GetName());
+    branch->SetName(tag.c_str());
+    branch->SetTitle(tag.c_str());
+    brun->detList[streams.size()-1]->SetName(tag.c_str());
+    brun->detList[streams.size()-1]->description=TString(tag.c_str());
+  }
+  printf("opened %ld files \n",streams.size());
+  return streams.size();
+}
+
+
+void closeFiles() 
+{
+  for(unsigned i=0; i<streams.size(); ++i ) streams[i]->close();
+}
+
+
+int main(int argc, char* argv[])
+{
+  cout << "executing " << argv[0] << endl;
+  if(argc<2) {
+    printf(" usage: convertRaw <tag> <events> \n ");
+    exit(0);
+   }
+  string stag = string(argv[1]);
+  TString tag(stag.c_str());
+  int nEvents=0;
+  if(argc>2) nEvents = atoi(argv[2]);
+
+  TFile* fout = new TFile(Form("rootData/%s.root",tag.Data()),"recreate");
+  brun = new TBRawRun(tag);
+  cout << " run name " << brun->GetName() << endl;
+  if(openFiles()<1) return 1;
+  while( streams[0]->good()) { 
+    readEvent(); 
+    if(int(brun->btree->GetEntries())%100==0) printf("... %lld \n",brun->btree->GetEntries() );
+  }
+  closeFiles();
+  fout->ls();
+  fout->Write();
+}
